70608e3241e59fc02fad08b57d7b4aef6e12b479
Shion Ichikawa <shion1305@gmail.com>
2023-09-14 02:24:27 +0900

    ✨ (4-VPCNetworking) GCI mynet-us-vm, mynet-eu-vm

--- 4-EssentialGoogleCloudInfrastructure-Foundation/VPCNetworking/basic.tf
+++ 4-EssentialGoogleCloudInfrastructure-Foundation/VPCNetworking/basic.tf
@@ -7,3 +7,37 @@ module "default-firewalls" {
   source       = "./modules/firewalls-default"
   network_name = google_compute_network.mynetwork.name
 }
+
+resource "google_compute_instance" "mynet-us-vm" {
+  name         = "mynet-us-vm"
+  machine_type = "e2-micro"
+  zone         = var.zone
+  network_interface {
+    network = google_compute_network.mynetwork.name
+    access_config {
+    }
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+}
+
+resource "google_compute_instance" "mynet-eu-vm" {
+  name         = "mynet-eu-vm"
+  machine_type = "e2-micro"
+  zone         = "europe-west1-c"
+  network_interface {
+    network = google_compute_network.mynetwork.name
+    access_config {
+    }
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+}

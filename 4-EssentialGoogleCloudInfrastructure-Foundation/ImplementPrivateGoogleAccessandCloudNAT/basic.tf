daf1dec54df0c178de47a24c1bca20061f840ac4
Shion Ichikawa <shion1305@gmail.com>
2023-09-15 08:44:36 +0900

    ✨ (4-ImplementPrivateGoogleAccess) complete Task1, create VM instance

---
+++ 4-EssentialGoogleCloudInfrastructure-Foundation/ImplementPrivateGoogleAccessandCloudNAT/basic.tf
@@ -0,0 +1,40 @@
+resource "google_compute_network" "privatenet" {
+  name                    = "privatenet"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "privatenet-us" {
+  name          = "privatenet-us"
+  ip_cidr_range = "10.130.0.0/20"
+  network       = google_compute_network.privatenet.self_link
+  region        = "us-central1"
+}
+
+resource "google_compute_firewall" "privatenet-allow-ssh" {
+  name    = "privatenet-allow-ssh"
+  network = google_compute_network.privatenet.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
+}
+
+resource "google_compute_instance" "vm-internal" {
+  machine_type = "e2-medium"
+  name         = "vm-internal"
+  zone         = "us-central1-c"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network    = google_compute_network.privatenet.self_link
+    subnetwork = google_compute_subnetwork.privatenet-us.self_link
+  }
+}
+
+
---
+++ 4-EssentialGoogleCloudInfrastructure-Foundation/ImplementPrivateGoogleAccessandCloudNAT/variable.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "The project ID to deploy to"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy to"
+  type        = string
+}
+
+variable "zone" {
+  description = "The zone to deploy to"
+  type        = string
+}

71b16da0327097c9751db9d629bef24e0ce5ea33
Shion Ichikawa <shion1305@gmail.com>
2023-09-16 14:17:06 +0900

    ✨ (4-ImplementPrivateGoogleAccess) CloudNAT config

--- 4-EssentialGoogleCloudInfrastructure-Foundation/ImplementPrivateGoogleAccessandCloudNAT/basic.tf
+++ 4-EssentialGoogleCloudInfrastructure-Foundation/ImplementPrivateGoogleAccessandCloudNAT/basic.tf
@@ -4,10 +4,10 @@ resource "google_compute_network" "privatenet" {
 }
 
 resource "google_compute_subnetwork" "privatenet-us" {
-  name          = "privatenet-us"
-  ip_cidr_range = "10.130.0.0/20"
-  network       = google_compute_network.privatenet.self_link
-  region        = "us-central1"
+  name                     = "privatenet-us"
+  ip_cidr_range            = "10.130.0.0/20"
+  network                  = google_compute_network.privatenet.self_link
+  region                   = "us-central1"
   private_ip_google_access = true
 }
 
@@ -43,4 +43,22 @@ resource "google_storage_bucket" "bucket-1" {
   storage_class = "MULTI_REGIONAL"
   location      = "us"
   force_destroy = true
+}
+
+resource "google_compute_router" "cloud-nat-router" {
+  name    = "nat-router"
+  network = google_compute_network.privatenet.self_link
+  region = "us-central1"
+}
+
+resource "google_compute_router_nat" "cloud-nat" {
+  name                               = "nat-config"
+  region                             = "us-central1"
+  router                             = google_compute_router.cloud-nat-router.name
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = google_compute_subnetwork.privatenet-us.self_link
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
 }
